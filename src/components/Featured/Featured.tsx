7eaf7f07b53c8a1c6540f68642069d527d0243d6
irenekyc <gugu_irene@hotmail.com>
2022-07-28 00:20:56 +0800

    fixed feature section styles

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -1,28 +1,30 @@
 import React from "react";
+import styles from "./Featured.module.scss";
 
 const Featured = () => {
   return (
-    <section className="featured-session">
-      <div className="featured-inner">
+    <section className={styles.ikFeaturedSession}>
+      <div className={styles.ikFeaturedSession__inner}>
         <div className="featured-heading">
           <i className="fab fa-react"></i>
           <i className="fab fa-node-js"></i>
         </div>
-        <div className="featured-title">
+        <div className={styles.ikFeaturedSession__inner__title}>
           <h5>
             {" "}
             <span>FEATURED </span>
           </h5>
-          <h1>MERN project - This portfolio</h1>
+          <h2 className={styles.ikFeaturedSession__inner__heading}>
+            MERN project - This portfolio
+          </h2>
         </div>
 
-        <div className="featured-image">
+        <div className={styles.ikFeaturedSession__inner__image}>
           <img src="images/my-portfolio.png" alt="portfolio" />
         </div>
-        <div className="featured-content">
-          <h4> MERN stands for MongoDB, Express, React and Node js. </h4>
+        <div className={styles.ikFeaturedSession__inner__content}>
+          <p> MERN stands for MongoDB, Express, React and Node js. </p>
           <p>
-            {" "}
             This portfolio is designed with MERN stack. MongoDB is mainly for
             storing and updating my project database. I use express and node to
             enable backend api routing to fetch data. React play an essential
@@ -30,7 +32,6 @@ const Featured = () => {
             enjoy my site!
           </p>
           <p>
-            {" "}
             Next step, the admin login panel and updating projects form are
             under construction. This will apply CRUD method
           </p>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,20 @@
 import React from "react";
+import styles from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-    <section className="hero-banner">
-      <div className="hero-banner-inner">
-        <div className="hero-banner-card" id="coding-hours">
-          <h1 className="leading"> 2500+</h1>
-          <p> coding hours</p>
-        </div>
-        <div className="hero-banner-card" id="learning-hours">
-          <h1 className="leading"> 300+</h1>
+    <section className={styles.ikHero__banner}>
+      <div className={styles.ikHero__banner__inner}>
+        <div className={styles.ikHero__banner__card} id="learning-hours">
+          <h3 className="leading"> 300+</h3>
           <p> learning hours</p>
         </div>
-        <div className="hero-banner-card" id="projects">
-          <h1 className="leading"> 60+</h1>
+        <div className={styles.ikHero__banner__card} id="coding-hours">
+          <h3 className="leading"> 2500+</h3>
+          <p> coding hours</p>
+        </div>
+        <div className={styles.ikHero__banner__card} id="projects">
+          <h3 className="leading"> 60+</h3>
           <p> coding projects</p>
         </div>
       </div>
